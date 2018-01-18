4c08527e00cdcd226533a1a0ee8d7fc006b9a34c
Victor Goudkov <vgoudk@gmail.com>
2018-01-18 20:19:33 +0300

    Repace default UI with grid view

--- src/main/java/my/vaadin/MyUI.java
+++ src/main/java/my/vaadin/MyUI.java
@@ -1,43 +1,50 @@
 package my.vaadin;
 
-import javax.servlet.annotation.WebServlet;
-
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
+import com.vaadin.ui.Grid;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import my.vaadin.app.Customer;
+import my.vaadin.app.CustomerService;
+
+import javax.servlet.annotation.WebServlet;
+import java.util.List;
 
 /**
- * This UI is the application entry point. A UI may either represent a browser window 
+ * This UI is the application entry point. A UI may either represent a browser window
  * (or tab) or some part of an HTML page where a Vaadin application is embedded.
  * <p>
- * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be
  * overridden to add component to the user interface and initialize non-component functionality.
  */
 @Theme("mytheme")
 public class MyUI extends UI {
 
+
+    private CustomerService service = CustomerService.getInstance();
+    private Grid<Customer> grid = new Grid<>(Customer.class);
+
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         final VerticalLayout layout = new VerticalLayout();
-        
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
 
-        Button button = new Button("Click Me");
-        button.addClickListener(e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
-        });
-        
-        layout.addComponents(name, button);
-        
+        grid.setColumns("firstName", "lastName", "email");
+
+        // add Grid to the layout
+        layout.addComponents(grid);
+        updateList();
+
+
         setContent(layout);
+    }
+
+    private void updateList() {
+        // fetch list of Customers from service and assign it to Grid
+        List<Customer> customers = service.findAll();
+        grid.setItems(customers);
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
